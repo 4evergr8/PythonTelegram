e1d56fc830e3964c532662aa17bdf56359949238
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-07-28 16:22:09 +0800

    Update webhook.py

--- api/webhook.py
+++ api/webhook.py
@@ -1,19 +1,14 @@
-from flask import Flask, request
 import requests
-
-app = Flask(__name__)
 
 
 BOT_TOKEN = "8241984939:AAF8JilTNlf7ucl5s8JB2z3EzBiRYqFFx48"
 
 
-@app.route("/", methods=["POST"])
-def webhook():
+def handler(request):
 
-    data = request.json
+    data = request.json()
 
     if "message" in data:
-
         chat_id = data["message"]["chat"]["id"]
 
         requests.post(
@@ -24,9 +19,7 @@ def webhook():
             }
         )
 
-    return "ok"
-
-
-@app.route("/", methods=["GET"])
-def index():
-    return "running"
+    return {
+        "statusCode": 200,
+        "body": "ok"
+    }
